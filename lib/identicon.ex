3736f976db2ea897fea322f54a3503064ff5a91d
erfan1995 <aliriza.erfan@gmail.com>
2024-05-29 21:34:40 -0600

    created struct, built grid, mirrored row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,12 +4,32 @@ defmodule Identicon do
   """
   def main(input) do
     input
-    |> hash_input()
+    |> hash_input
+    |> pick_color
+    |> build_grid
 
   end
 
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
   end
 end
